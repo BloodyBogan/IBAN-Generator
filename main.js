ace80bc13efb55347994beda6b951967c43b3125
Michal Kaštan <41683416+BloodyBogan@users.noreply.github.com>
2018-10-11 22:56:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ var display = document.getElementById('ucet');
 
 var ibanGenerator = function() {
 
-  first = Math.floor(Math.random() * 100);
-  second = Math.floor(Math.random() * 10000);
-  third = Math.floor(Math.random() * 10000);
+  first = Math.floor(Math.random() * 90) + 10;
+  second = Math.floor(Math.random() * 100);
+  third = Math.floor(Math.random() * 10);
+  fourth = Math.floor(Math.random() * 10000);
+  fifth = Math.floor(Math.random() * 10000);
+  sixth = Math.floor(Math.random() * 10000);
 
-  generatedIban = first + ' ' + second + ' ' + third
+  generatedIban = first + ' 0' + second + '0 ' + '000' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth
 
 }
 
